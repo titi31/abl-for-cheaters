1721a3f69bd4dc989a2b939764e5aa6b37f9a5f7
Thierry Laude <thlaude@airfrance.fr>
2013-06-20 17:57:30 +0200

    Changer couleur de texte boutons de l'écran d'accueil (ABLONE-85)
    https://www9.ute1.klm.com/corporate/pse/jira/browse/ABLONE-85

--- AngryUTBM/src/view/MenuHomeView.java
+++ AngryUTBM/src/view/MenuHomeView.java
@@ -9,6 +9,7 @@ public class MenuHomeView extends GameViewMenu
 	//declaration des attributs adaptes a la page principale
 	private JButton newButton,loadButton,optionsButton,exitButton;
 	private Color AFBlueColor = new Color(12,25,74);
+	private Color KLMBlueColor = new Color(114,208,241);
 
 	
 	public MenuHomeView() {
@@ -17,21 +18,25 @@ public class MenuHomeView extends GameViewMenu
         newButton.setSize(200,30);
         newButton.setLocation(frameWidth/2-100, 150);
         newButton.setBackground(AFBlueColor);
+        newButton.setForeground(KLMBlueColor);
         
         loadButton = new JButton("LOAD GAME");
         loadButton.setSize(200,30);
         loadButton.setLocation(frameWidth/2-100, 225);
         loadButton.setBackground(AFBlueColor);
+        loadButton.setForeground(KLMBlueColor);
         
         optionsButton = new JButton("CONTROLS");
         optionsButton.setSize(200,30);
         optionsButton.setLocation(frameWidth/2-100, 300);
         optionsButton.setBackground(AFBlueColor);
+        optionsButton.setForeground(KLMBlueColor);
         
         exitButton = new JButton("EXIT GAME");
         exitButton.setSize(200,30);
         exitButton.setLocation(frameWidth/2-100, 375);
         exitButton.setBackground(AFBlueColor);
+        exitButton.setForeground(KLMBlueColor);
         backButton.setVisible(false); //pas de bouton de retour sur la page principale
         
         //ajout des attributs avec l'index 1 pour etre en premier plan
